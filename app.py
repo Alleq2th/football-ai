262dfb9f402a84b78687c610f4c7f0d5b6db0f3e
Alleq2th <allengreg371@gmail.com>
2026-06-06 10:40:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 from news_fetcher import get_news
 from content_generator import generate_content
 from story_ranker import score_story
+from graphic_generator import create_graphic
 
 app = Flask(__name__)
 
@@ -10,7 +11,6 @@ def home():
 
     news = get_news()
 
-    # Sort stories by ranking score
     news = sorted(
         news,
         key=lambda story: score_story(story["title"]),
@@ -22,8 +22,8 @@ def home():
     for story in news[:5]:
 
         content = generate_content(
-    story["title"],
-    story["link"]
+            story["title"],
+            story["link"]
         )
 
         posts.append({
@@ -36,6 +36,16 @@ def home():
     return render_template(
         "index.html",
         posts=posts
+    )
+
+@app.route("/generate_graphic/<path:headline>")
+def generate_graphic(headline):
+
+    filename = create_graphic(headline)
+
+    return send_file(
+        filename,
+        as_attachment=True
     )
 
 if __name__ == "__main__":
